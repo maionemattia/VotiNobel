42cc7156d0e37874e14a68755daffd1a7dff23a0
mattiamaione <mattiamaione@macbook-pro.homenet.telecomitalia.it>
2020-04-21 10:54:41 +0200

    Videolezione Voti Nobel

--- src/main/java/it/polito/tdp/nobel/FXMLController.java
+++ src/main/java/it/polito/tdp/nobel/FXMLController.java
@@ -36,7 +36,19 @@ public class FXMLController {
     void doCalcolaCombinazione(ActionEvent event) {
     		try {
     			int numeroCrediti = Integer.parseInt(txtInput.getText());
+    			Long start = System.currentTimeMillis();
     			Set<Esame> voti = model.calcolaSottoinsiemeEsami(numeroCrediti);
+    			Long end = System.currentTimeMillis();
+    			
+    			if(voti == null) {
+    				txtResult.appendText("Non ho trovato soluzioni");
+    				return;
+    			}
+    			txtResult.appendText("Tempo impiegato: " + (end-start) + "ms\n");
+    			txtResult.appendText("Media: " + this.model.calcolaMedia(voti) + "\n");
+    			for(Esame e : voti) {
+    				txtResult.appendText(e.toString() + "\n");
+    			}
     			
     		} catch (NumberFormatException e) {
     			txtResult.setText("Inserire un numero di crediti > 0");
--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -1,15 +1,110 @@
 package it.polito.tdp.nobel.model;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import it.polito.tdp.nobel.db.EsameDAO;
 
 public class Model {
 
-	
+	private List<Esame> esami;
+	private Set<Esame> ottimo;
+	private double mediaBest;
+
+	public Model() {
+		EsameDAO dao = new EsameDAO();
+		this.esami = dao.getTuttiEsami();
+	}
+
 	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
-		
-		System.out.println("TODO!");
-		
-		return null;
+
+		Set<Esame> parziale = new HashSet<>();
+		cerca2(parziale, 0, numeroCrediti);
+
+		return ottimo;
+	}
+
+	private void cerca(Set<Esame> parziale, int L, int m) {
+
+		int crediti = sommaCrediti(parziale);
+
+		// Caso TERMINALE
+		if (crediti > m)
+			return;
+
+		if (crediti == m) {
+			double media = calcolaMedia(parziale);
+			if (media > mediaBest) {
+				ottimo = new HashSet<>(parziale);
+				mediaBest = media;
+			}
+		}
+
+		if (L == esami.size()) {
+			return;
+		}
+
+		// GENERAZIONE SOTTOPROBLEMA
+
+		// provo ad aggiungerlo
+		parziale.add(esami.get(L));
+		cerca(parziale, L + 1, m);
+		parziale.remove(esami.get(L));
+
+		// provo a non aggiungerlo
+		cerca(parziale, L + 1, m);
+
+	}
+
+	private void cerca2(Set<Esame> parziale, int L, int m) {
+
+		int crediti = sommaCrediti(parziale);
+
+		// Caso TERMINALE
+		if (crediti > m)
+			return;
+
+		if (crediti == m) {
+			double media = calcolaMedia(parziale);
+			if (media > mediaBest) {
+				ottimo = new HashSet<>(parziale);
+				mediaBest = media;
+			}
+		}
+
+		if (L == esami.size()) {
+			return;
+		}
+
+		// GENERAZIONE SOTTOPROBLEMA
+
+		for (Esame e : esami) {
+			if (!parziale.contains(e)) {
+				parziale.add(e);
+				cerca2(parziale, L + 1, m);
+				parziale.remove(e);
+			}
+		}
+
+	}
+
+	public double calcolaMedia(Set<Esame> parziale) {
+		int somma = 0;
+		int crediti = 0;
+		for (Esame e : parziale) {
+			crediti += e.getCrediti();
+			somma += (e.getVoto() * e.getCrediti());
+		}
+		return somma / crediti;
+	}
+
+	private int sommaCrediti(Set<Esame> parziale) {
+		int sommaCrediti = 0;
+		for (Esame e : parziale) {
+			sommaCrediti += e.getCrediti();
+		}
+		return sommaCrediti;
 	}
 
 }
